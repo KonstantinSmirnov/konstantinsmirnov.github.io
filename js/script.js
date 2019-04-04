1670bc256364518088bc7392e951f64284839b10
Konstantin <rusksmi@gmail.com>
2019-04-04 11:54:11 +0000

    Add random colour generator

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var btn = document.getElementById('change');
+var text = document.getElementById('colour');
+
+var generator = function() {
+  newColour = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  console.log(newColour.length);
+  if(newColour.length < 7) {
+    generator();
+  }
+}
+
+btn.addEventListener('click', function() {
+    generator();
+    
+    document.body.style.background = newColour;
+    btn.style.color = newColour;
+    text.innerText = newColour;
+});
+
+// dh = $(document).height();
+
+// $('#container').css({'padding-top':dh/3});
+
+// $('#change').click(function() {
+//     newColour = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+//     $('body').css({'background':newColour});
+//     $('#change').css({'color':newColour});
+//     $('#colour').text(newColour);
+// });
